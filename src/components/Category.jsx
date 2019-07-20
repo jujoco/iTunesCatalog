429be8ecb3b2e6f60f68ffff895204a4cc1e3662
Julian cobos <jujocotm@gmail.com>
2019-07-20 03:36:23 -0700

    implement Table and reactstrap styles

---
+++ src/components/Category.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Item from './Item';
+import { Table } from 'reactstrap';
+
+const Category = ({ category, sectionName, handleFavorite }) => {
+  return (
+    <div className="Category">
+      <h2>{sectionName}</h2>
+      <Table className="Table">
+        <thead>
+          <tr scope="row">
+            <th>Artwork</th>
+            <th>Save</th>
+            <th>Name</th>
+            <th>Genre</th>
+            <th>Link to iTunes</th>
+          </tr>
+        </thead>
+        <tbody>
+          {category.map((item, i) => {
+            return <Item type={sectionName} details={item} handleFavorite={handleFavorite} key={i} />;
+          })}
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+
+export default Category;
